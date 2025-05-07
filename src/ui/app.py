9bda1b8d256a09a2bbba88c3c16e670e9b15a83a
caninecrew <samuelleerumbley@gmail.com>
2025-05-06 23:17:50 -0500

    Refactor app.py to update imports and method names for WordleGame and WordList

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,14 +1,14 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from src.word_list import WordList
-from src.game import Game
+from ..word_list import WordList
+from ..game import WordleGame  # Changed from Game to WordleGame
 
 class WordleGameUI(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(orientation='vertical', **kwargs)
         self.word_list = WordList()
-        self.answer = self.word_list.get_random_answer()
-        self.game = Game(self.answer)
+        self.answer = self.word_list.get_random_word()  # Changed from get_random_answer to get_random_word
+        self.game = WordleGame(self.answer)  # Changed from Game to WordleGame
         # TODO: Build grid, input, feedback UI
 
 class WordleApp(App):
